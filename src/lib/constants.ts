da0fa67a07a3171f541105057ceac87c08bffe51
Andreas Lordos <andreasglordos@gmail.com>
2024-07-21 17:24:12 -0700

    upated link

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const GAME_TITLE = "Decades";
 export const GAME_DESCRIPTION = "Group the headlines by decade!"
 export const MAX_MISTAKES = 5;
-export const SHARE_URL = "decades.lordos.tech";
+export const SHARE_URL = "https://decades.lordos.tech";
 export const START_DATE = new Date('2024-07-21');
